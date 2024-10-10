299481d00c2f6e2ba6c35319586ba293e4f84c13
foxytanuki <biz@foxytanuki.dev>
2024-10-10 18:06:49 +0900

    update contract deploy scirpt

--- src/contracts/deploy.sh
+++ src/contracts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+RPC_URL="https://rpc.toliman.suave.flashbots.net"
+PRIVATE_KEY="91ab9a7e53c220e6210460b65a7a3bb2ca181412a8a7b43ff336b3df1737ce12"
 
-address=$(forge create --json --private-key 91ab9a7e53c220e6210460b65a7a3bb2ca181412a8a7b43ff336b3df1737ce12 src/Counter.sol:Counter | jq .deployedTo | tr -d '"')
+address=$(forge create --json --rpc-url $RPC_URL --private-key $PRIVATE_KEY src/Counter.sol:Counter --legacy | jq .deployedTo | tr -d '"')
 
 echo '{"address": "'$address'"}' > ./deployment.json
 echo "deployed to $address"
